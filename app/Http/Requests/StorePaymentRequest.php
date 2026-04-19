7ccd3a40624cbb162188dd657c818ca7560639f8
nanaya <itsnanaya.id@gmail.com>
2026-04-19 14:46:42 +0700

    feat: add FormRequest classes for household, pickup, and payment validation

---
+++ app/Http/Requests/StorePaymentRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StorePaymentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'household_id' => 'required|string',
+            'amount'       => 'required|numeric|min:0',
+        ];
+    }
+}
